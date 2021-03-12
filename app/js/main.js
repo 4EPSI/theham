704e84b45fd99c7b07250a51ba68632c886149c5
4EPSI <mnatsakanyan@yual.io>
2021-03-12 21:39:05 +0200

    mob menu

--- app/js/main.js
+++ app/js/main.js
@@ -2,12 +2,12 @@
 $('.burger').on('click', function () {
     $('.burger').toggleClass('active');
     $('body').toggleClass('no_scroll');
-    $('.menu_mob').toggleClass('active');
+    $('.header_menu').toggleClass('active');
 });
 $(document).on('click', function (e) {
     if (!$(e.target).closest('.header').length) {
         $('.burger').removeClass('active');
-        $('.menu_mob').removeClass('active');
+        $('.header_menu').removeClass('active');
         $('body').removeClass('no_scroll');
     }
     e.stopPropagation();
